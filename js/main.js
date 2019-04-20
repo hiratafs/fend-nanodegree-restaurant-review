a21b324a42fabe50498fcaaa739a0165f6f397e4
Tatiane Hirata <tatihirata@outlook.com>
2019-04-19 20:08:27 -0400

    Mapbox key adicionado

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoidm9yc3BpZXIiLCJhIjoiY2p1ZDRmZjh2MGs1MTN5bzYydDVrOGZjcyJ9.weQoZp30LL6axVIUYGmBIw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +

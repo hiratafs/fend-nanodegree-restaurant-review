a0a9116acc0e619093968924708f7525558001e6
hiratafs <tatihirata@outlook.com>
2019-05-03 13:25:55 -0400

    Criado arquivo do service worker

---
+++ js/sw.js
@@ -0,0 +1,13 @@
+
+if('serviceWorker' in navigator) {
+    console.log("alguma coisa")    
+} else {
+    console.log("deu erro!");
+}
+
+//self.addEventListener('install', function(event) {
+    
+//})
+//self.addEventListener('', function() {
+
+//})

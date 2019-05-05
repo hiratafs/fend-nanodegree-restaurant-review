bc8bfa50a847d48a80806b0ae18be189cc809aa5
Tatiane Hirata <tatihirata@outlook.com>
2019-05-04 21:50:33 -0400

    Criação dos eventos e inserção dos nomes dos arquivos para cache

--- sw.js
+++ sw.js
@@ -1,19 +1,49 @@
-if('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js')
-        .then(function() {
-            console.log('Serviço registrado')
+const nomeCache = "versaocache"
+
+const cacherecursos = [
+    './',
+    './css/styles.css',
+    './data/restaurants.json',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg',
+    './js/dbhelper.js',
+    './js/main.js',
+    './js/restaurant_info.js',
+    './js/serviceworker_reg.js',
+    './index.html',
+    './restaurant.html',
+    'https://fonts.googleapis.com/css?family=Rubik:400,400i,500,700,700i'
+];
+
+self.addEventListener('install', function(e) {
+
+    e.waitUntil(
+        caches.open(nomeCache).then(function(cache) {
+            console.log("Cache funcionando!");
+            cache.addAll(cacherecursos);
+         });
+         console.log("[service worker] installed");
+
+    )
+});
+
+self.addEventListener('activate', function(e) {
+    console.log("[service worker] activated")
+    e.waitUntil(
+        caches.keys().then(function(nomeCache) {
+            return Promise.all()
         })
-        .catch(function(err) {
-            console.log("Falhou!", err)
-    });
-    })
-}
+    )
+});
 
-
-navigator.serviceWorker.register('sw.js')
-.then(function(){
-    console.log("Yay!") })
-.catch(function(){
-    console.log("Falhou!");
-})
+self.addEventListener('fetch', function(e) {
+    console.log("[service worker] fetched")
+});

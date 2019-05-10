7e0a70e7d64a81508513cb2689af957530268d72
hiratafs <tatihirata@outlook.com>
2019-05-10 15:31:02 -0400

    Inserido tabindex no nome do restaurante e retirado da attribution do leaflet

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -20,14 +20,14 @@ initMap = () => {
         center: [restaurant.latlng.lat, restaurant.latlng.lng],
         zoom: 16,
         scrollWheelZoom: false,
-        keyboard: true
+        keyboard: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
         mapboxToken: 'pk.eyJ1Ijoidm9yc3BpZXIiLCJhIjoiY2p1ZDRmZjh2MGs1MTN5bzYydDVrOGZjcyJ9.weQoZp30LL6axVIUYGmBIw',
         maxZoom: 18,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/" tabindex="-1>OpenStreetMap</a> contributors, ' +
+          '<a href="https://creativecommons.org/licenses/by-sa/2.0/" tabindex="-1>CC-BY-SA</a>, ' +
+          'Imagery © <a href="https://www.mapbox.com/" tabindex="-1>Mapbox</a>',
         id: 'mapbox.streets'    
       }).addTo(newMap);
       fillBreadcrumb();

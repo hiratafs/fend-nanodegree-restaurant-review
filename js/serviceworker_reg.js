aba460ef0b13a8970e6ff59536f44b70a87c499f
Tatiane Hirata <tatihirata@outlook.com>
2019-05-11 19:29:29 -0400

    Evento load no arquivo do service worker e aria no index.html

--- js/serviceworker_reg.js
+++ js/serviceworker_reg.js
@@ -1,8 +1,11 @@
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-    .then(function(reg){
-        console.log("Tudo Ok!", reg) })
-    .catch(function(err){
-        console.log("Falhou!", err);
+    window.addEventListener('load', function(){
+        navigator.serviceWorker.register('./sw.js')
+            .then(function(reg){
+                console.log("Tudo Ok!", reg) })
+            .catch(function(err){
+                console.log("Falhou!", err);
+        })
     })
+  
 }
